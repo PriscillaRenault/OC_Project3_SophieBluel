b21a6de199fc540525eb07c6f6f1be5edba08b79
PriscillaRenault <priscilla.renault72460@gmail.com>
2024-06-20 14:51:50 +0200

    show gallery without reload

--- FrontEnd/JS/modal.js
+++ FrontEnd/JS/modal.js
@@ -1,42 +1,150 @@
 import { fetchWorks, deleteWork } from "./api.js";
+import { createGallery } from "./galleryEdit.js";
 
+let titleDialog;
+let headerModal;
+let closeBtn;
+let returnArrow;
+let works;
 // Fonction de fermeture de la modal
 export const closeModal = (event) => {
 	event.preventDefault();
 	const dialog = document.querySelector("#js-dialog");
 	dialog.close();
-	window.location.href = "./homepage_edit.html";
+	dialog.classList.remove("dialog");
+	dialog.classList.add("hidden");
 };
 
 // Fonction de base pour initialiser la modal
 export const baseModal = () => {
 	const dialog = document.querySelector("#js-dialog");
+	dialog.classList.remove("hidden");
 	dialog.classList.add("dialog");
 	dialog.innerHTML = "";
 
-	const closeBtn = document.createElement("span");
+	headerModal = document.createElement("div");
+	headerModal.classList.add("headerModal");
+
+	returnArrow = document.createElement("span");
+	returnArrow.innerHTML = '<i class="fa-solid fa-arrow-left"></i>';
+	returnArrow.classList.add("returnArrow");
+
+	closeBtn = document.createElement("span");
 	closeBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
 	closeBtn.classList.add("xmark");
 	closeBtn.addEventListener("click", closeModal);
 
-	const titleDialog = document.createElement("h2");
+	titleDialog = document.createElement("h2");
 	titleDialog.textContent = "";
 
-	dialog.appendChild(closeBtn);
+	dialog.appendChild(headerModal);
+	headerModal.appendChild(returnArrow);
+	headerModal.appendChild(closeBtn);
 	dialog.appendChild(titleDialog);
+};
+
+const updateReturnArrow = () => {
+	const returnArrow = document.querySelector(".returnArrow");
+	if (returnArrow.parentElement.classList.contains("headerGallery")) {
+		returnArrow.innerHTML = "";
+	} else {
+		returnArrow.innerHTML = '<i class="fa-solid fa-arrow-left"></i>';
+		returnArrow.addEventListener("click", createModalGallery);
+	}
+};
+
+// Fonction pour créer la modal de galerie
+export const createModalGallery = async () => {
+	baseModal();
+	const dialog = document.querySelector("#js-dialog");
+	headerModal.classList.add("headerGallery");
+	titleDialog = dialog.querySelector("h2");
+	titleDialog.textContent = "Galerie photo";
+
+	// Récupérer les travaux via l'API
+	works = await fetchWorks();
+
+	const galleryMini = document.createElement("div");
+	galleryMini.classList.add("gallery-mini");
+	galleryMini.id = "js-galleryMini";
+
+	works.forEach((project) => {
+		const projectElement = document.createElement("figure");
+		projectElement.dataset.id = project.id;
+
+		const projectImg = document.createElement("img");
+		projectImg.src = project.imageUrl;
+		projectImg.alt = project.title;
+
+		const trashIcon = document.createElement("span");
+		trashIcon.classList.add("js-trashIcon", "js-trashIcon");
+		// trashIcon.setAttribute("dataset", trashIcon);
+		trashIcon.innerHTML =
+			'<i class="fa-solid fa-trash-can js-trashIcon trashIcon"></i>';
+
+		galleryMini.appendChild(projectElement);
+		projectElement.appendChild(projectImg);
+		projectElement.appendChild(trashIcon);
+	});
+
+	dialog.appendChild(galleryMini);
+	updateReturnArrow();
+
+	galleryMini.addEventListener("click", async (event) => {
+		if (event.target.classList.contains("js-trashIcon")) {
+			event.preventDefault();
+			const projectElement = event.target.closest("figure");
+			const id = projectElement.dataset.id;
+			const token = sessionStorage.getItem("token");
+			if (!token) {
+				console.log("No token");
+				return;
+			}
+
+			try {
+				const response = await deleteWork(id, token);
+				if (response.ok) {
+					projectElement.remove();
+					updateMainGallery();
+				} else {
+					console.log(
+						`Error ${response.status} ${response.statusText}`
+					);
+				}
+			} catch (error) {
+				console.error("Error during fetch: ", error);
+			}
+		}
+	});
+
+	const btnAddPhoto = document.createElement("input");
+	btnAddPhoto.type = "submit";
+	btnAddPhoto.value = "Ajouter une photo";
+	dialog.appendChild(btnAddPhoto);
+	btnAddPhoto.addEventListener("click", (event) => {
+		event.preventDefault();
+		createModalAddPhoto();
+	});
+};
+
+const updateMainGallery = async () => {
+	const gallery = document.querySelector("#js-gallery");
+	gallery.innerHTML = "";
+	works = await fetchWorks();
+	createGallery(works);
 };
 
 // Fonction pour créer la modal d'ajout de photo
 export const createModalAddPhoto = () => {
 	baseModal();
 	const dialog = document.querySelector("#js-dialog");
-
-	const titleDialog = document.createElement("h2");
-	titleDialog.textContent = "Ajout photo";
+	titleDialog = dialog.querySelector("h2");
+	titleDialog.textContent = "Ajout Photo";
 	dialog.appendChild(titleDialog);
 
 	const formAddPhoto = document.createElement("form");
 	formAddPhoto.classList.add("formAddPhoto");
+	formAddPhoto.id = "js-formAddPhoto";
 
 	const containerPhoto = document.createElement("div");
 	containerPhoto.classList.add("containerPhoto");
@@ -84,7 +192,7 @@ export const createModalAddPhoto = () => {
 
 	const btnSubmit = document.createElement("input");
 	btnSubmit.type = "submit";
-	btnSubmit.value = "Envoyer";
+	btnSubmit.value = "Valider";
 	btnSubmit.classList.add("invalid");
 
 	formAddPhoto.appendChild(containerPhoto);
@@ -103,74 +211,6 @@ export const createModalAddPhoto = () => {
 
 	dialog.appendChild(formAddPhoto);
 	dialog.appendChild(btnSubmit);
-};
 
-// Fonction pour créer la modal de galerie
-export const createModalGallery = async () => {
-	baseModal();
-	const dialog = document.querySelector("#js-dialog");
-	const titleDialog = dialog.querySelector("h2");
-	titleDialog.textContent = "Galerie photo";
-
-	// Récupérer les travaux via l'API
-	const works = await fetchWorks();
-
-	const galleryMini = document.createElement("div");
-	galleryMini.classList.add("gallery-mini");
-	galleryMini.id = "js-galleryMini";
-
-	works.forEach((project) => {
-		const projectElement = document.createElement("figure");
-		projectElement.dataset.id = project.id;
-
-		const projectImg = document.createElement("img");
-		projectImg.src = project.imageUrl;
-		projectImg.alt = project.title;
-
-		const trashIcon = document.createElement("span");
-		trashIcon.classList.add("js-trashIcon", "trashIcon");
-		trashIcon.setAttribute("dataset", trashIcon);
-		trashIcon.innerHTML = '<i class="fa-solid fa-trash-can trashIcon"></i>';
-
-		projectElement.appendChild(projectImg);
-		projectElement.appendChild(trashIcon);
-		galleryMini.appendChild(projectElement);
-	});
-
-	dialog.appendChild(galleryMini);
-
-	galleryMini.addEventListener("click", async (event) => {
-		if (event.target.classList.contains("trashIcon")) {
-			event.preventDefault();
-			const projectElement = event.target.closest("figure");
-			const id = projectElement.dataset.id;
-			const token = sessionStorage.getItem("token");
-			if (!token) {
-				console.log("No token");
-				return;
-			}
-
-			try {
-				const response = await deleteWork(id, token);
-				if (response.ok) {
-					projectElement.remove();
-				} else {
-					console.log(
-						`Error ${response.status} ${response.statusText}`
-					);
-				}
-			} catch (error) {
-				console.error("Error during fetch: ", error);
-			}
-		}
-	});
-
-	const btnAddPhoto = document.createElement("input");
-	btnAddPhoto.type = "submit";
-	btnAddPhoto.value = "Ajouter une photo";
-	dialog.appendChild(btnAddPhoto);
-	btnAddPhoto.addEventListener("click", (event) => {
-		event.preventDefault();
-		createModalAddPhoto();
-	});
+	updateReturnArrow();
 };
