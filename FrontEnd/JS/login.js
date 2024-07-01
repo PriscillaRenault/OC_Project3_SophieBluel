ecf78e9f5dcbcce3f36a48adf584284d30825cf6
PriscillaRenault <priscilla.renault72460@gmail.com>
2024-07-01 10:26:45 +0200

    resolve a  bug on connection page

--- FrontEnd/JS/login.js
+++ FrontEnd/JS/login.js
@@ -1,9 +1,11 @@
-email = document.querySelector("#js-email");
-password = document.querySelector("#js-password");
+const email = document.querySelector("#js-email");
+const password = document.querySelector("#js-password");
+
 document
 	.querySelector("#js-btnConnect")
 	.addEventListener("click", async (e) => {
 		e.preventDefault();
+
 		const response = await fetch("http://localhost:5678/api/users/login", {
 			method: "POST",
 			headers: {
@@ -14,7 +16,18 @@ document
 				password: password.value,
 			}),
 		});
+
+		// Vérifiez le statut de la réponse
+		if (!response.ok) {
+			console.error(
+				`HTTP error! status: ${response.status}, text: ${response.statusText}`
+			);
+			alert("E-mail ou mot de passe incorrect");
+			return;
+		}
+
 		const data = await response.json();
+
 		if (data.error) {
 			alert("E-mail ou mot de passe incorrect");
 		} else {
